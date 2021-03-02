aff1fb2b0cb700688a7b22faeb22b3e4af5cbbe1
JTLin-nzxt <jt.lin@nzxt.com>
2021-03-02 14:44:33 +0800

    Update webhook.js

--- scripts/webhook.js
+++ scripts/webhook.js
@@ -3,21 +3,8 @@ module.exports = function(robot)
 	robot.router.get('/hubot/test/:room', function(req, res) 
     {
 		console.log("root attack : " + req.params.room);
+		robot.messageRoom("G01P1JPCMEG", "Webhook!");
 		res.send('OK');
     });
-	
-    robot.router.get('/hubot/acumatica/:room', function(req, res) 
-    {
-		//room = "G01P1JPCMEG";
-		room   = req.params.room;
-		try{
-		data = req.body;
-		console.log(data);
-		robot.messageRoom(room, "Acumatica Webhook :" + data);
-		res.send('OK');
-		}
-		catch(Exception e){
-			res.send(e);
-		}
-    });
+
 }
