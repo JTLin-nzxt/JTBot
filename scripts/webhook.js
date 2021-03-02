1221260edbf64453d86ef6d87b260bc6adabfa94
JTLin-nzxt <jt.lin@nzxt.com>
2021-03-02 14:51:52 +0800

    Update webhook.js

--- scripts/webhook.js
+++ scripts/webhook.js
@@ -1,8 +1,7 @@
 module.exports = function(robot) 
 {
-	robot.router.get('/hubot/test/:room', function(req, res) 
+	robot.router.get('/hubot/test/', function(req, res) 
     {
-		console.log("root attack : " + req.params.room);
 		robot.messageRoom("G01P1JPCMEG", "Webhook!");
 		res.send('OK');
     });
