b76e3fbe341ea21af1661566bab96ae7fe2835fe
JTLin-nzxt <jt.lin@nzxt.com>
2021-03-02 13:39:51 +0800

    Update webhook.js

--- scripts/webhook.js
+++ scripts/webhook.js
@@ -3,7 +3,7 @@ module.exports = function(robot)
     robot.router.post('/acumaticaWebhook/', function(req, res) 
     {
 		room = "G01P1JPCMEG";
-		data = if req.body.payload? then JSON.parse req.body.payload else req.body;
+		data = req.body;
 		console.log(data);
 		robot.messageRoom(room, "I have a secret: #{data}");
 		res.send('OK');
